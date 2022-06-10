124fa01a9aab32d6fdc6381e8828747d84956ff0
JohnOK <john.ok.esp@gmail.com>
2022-06-10 02:46:23 +0200

    Added options to add and delete students.

--- classes/school.py
+++ classes/school.py
@@ -10,9 +10,17 @@ class School:
     def list_students(self):
         print('\n')
         for i, student in enumerate(self.students):
-            print(f'{i + 1}. {student.name} {student.school_id}')
+            print(f'{i + 1}. {student.name.title()} {student.school_id}')
 
     def find_student_by_id(self, student_id):
         for student in self.students:
             if student.school_id == student_id:
                 return student
+
+    def add_student(self, student_data):
+        self.students.append(Student(**student_data))
+
+    def delete_student(self, student_id):
+        for i, student in enumerate(self.students):
+            if student.school_id == student_id:
+                del self.students[i]
--- runner.py
+++ runner.py
@@ -11,5 +11,17 @@ while True:
         student_id = input('Enter student id:')
         student_string = str(school.find_student_by_id(student_id))
         print(student_string)
+    elif mode == '3':
+        student_data = {'role':'student'}
+        student_data['name'] = input("Enter student's name:\n")
+        student_data['age'] = input("Enter student's age:\n")
+        student_data['school_id'] = input("Enter student's id number:\n")
+        student_data['password'] = input("Enter student's password:\n")
+
+        school.add_student(student_data)
+    elif mode == '4': #Remove a student <studen_id>
+        student_id = input('Enter student id:')
+
+        school.delete_student(student_id)
     elif mode == '5':
         break
